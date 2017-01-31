6fe634824b0b064faf3decc796f3ece7a5fc3383
Nicole Siegel <nsiegel2@gmail.com>
2017-01-31 14:39:19 -0500

    move video.js functions to Video class

---
+++ js/classes/Video.js
@@ -0,0 +1,27 @@
+var Video = function(url) {
+  this.id = this.getVideoId(url);
+  this.player = this.loadVideo();
+};
+
+Video.prototype = {
+  loadVideo: function() {
+    // Replace the 'ytplayer' element with an <iframe> and
+    // YouTube player after the API code downloads.
+    return new YT.Player('ytplayer', {
+      height: '350',
+      width: '550',
+      videoId: this.id
+    });
+  },
+  getVideoId: function(url) {
+    var id = '';
+    url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+    if(url[2] !== undefined) {
+      id = url[2].split(/[^0-9a-z_\-]/i);
+      id = id[0];
+    } else {
+      id = url;
+    }
+    return id;
+  }
+}
--- js/video.js
+++ js/video.js
@@ -4,7 +4,7 @@ var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-var player;
+var video;
 
 // On click load
 document.getElementById('get-video').onclick = getVideo;
@@ -12,30 +12,9 @@ document.getElementById('get-video').onclick = getVideo;
 var videoId;
 
 function getVideo() {
-  videoId = getVideoId();
-  return loadVideo();
-}
-
-function getVideoId() {
   var url = document.getElementById('yt-url').value;
-  var id = '';
-  url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
-  if(url[2] !== undefined) {
-    id = url[2].split(/[^0-9a-z_\-]/i);
-    id = id[0];
-  }
-  else {
-    id = url;
-  }
-  return id;
+  video = new Video(url);
+  return video.player;
 }
 
-function loadVideo() {
-  // Replace the 'ytplayer' element with an <iframe> and
-  // YouTube player after the API code downloads.
-  player = new YT.Player('ytplayer', {
-    height: '350',
-    width: '550',
-    videoId: videoId
-  });
 }
