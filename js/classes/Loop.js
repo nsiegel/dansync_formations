6fb49600b9d6833cf6800b97af3d2b655c38184e
Nicole Siegel <nsiegel2@gmail.com>
2017-02-20 15:30:17 -0500

    update functions in loop class

--- js/classes/Loop.js
+++ js/classes/Loop.js
@@ -1,19 +1,49 @@
 // game loop
-var Loop = function() {};
+var Loop = function() {
+  this.running = false;
+  this.loop = null;
+};
 
 Loop.prototype = {
-  run: function() {
+  run: function(canvas, formationTimeline) {
     // this fn runs a loop that will update the program state and then
-    // render(as in draw) the current models to screen
+    // render the current models to screen
+    var self = this;
+    self.running = true;
+    var formations = formationTimeline.formations;
+    var transitions = formationTimeline.transitions;
+    var video = formationTimeline.video;
+    var i = 0;
+
+    self.loop = setInterval(function () {
+      if (i >= formations.length) {
+        self.stop();
+        return;
+      }
+      var currTime = video.getCurrentTime();
+      if (formations[i].time[0] <= currTime) {
+        self.render(canvas, formations[i]);
+      }
+      if (formations[i].time[1] <= currTime) {
+        self.render(canvas, transitions[i]);
+        i++;
+      }
+    }, 10);
   },
   stop: function() {
     // stop the loop so we can pause it
+    if (this.running) {
+      clearInterval(this.loop);
+      this.running = false;
+    }
   },
-  update: function() {
-    // update internal logic such as requesting a new formation from the
-    // timeline whenever its needed so that render can print it
-  },
-  render: function() {
-    // draw the current formation from the timeline
+  render: function(canvas, formationOrTransition) {
+    // draw the current formation/transition from the timeline
+    if (formationOrTransition instanceof Transition) {
+      formationOrTransition.run(canvas);
+    }
+    else if (formationOrTransition instanceof Formation) {
+      canvas.drawFormation(formationOrTransition.spots);
+    }
   }
 };
