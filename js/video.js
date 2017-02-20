37e695fc5c7538f154976222af25296c20c489eb
Nicole Siegel <nsiegel2@gmail.com>
2017-02-20 15:36:05 -0500

    allow user to assign start and end times to formations

--- js/video.js
+++ js/video.js
@@ -6,8 +6,9 @@ var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var video;
 
-// On click load
 document.getElementById('get-video').onclick = getVideo;
+document.getElementById('start-time').onclick = setTime('start');
+document.getElementById('end-time').onclick = setTime('end');
 
 var videoId;
 
@@ -18,4 +19,33 @@ function getVideo() {
   return video.player;
 }
 
+function getCurrentTime() {
+  var time = video.getCurrentTime();
+  return time;
+}
+
+function setTime(startOrEnd) {
+  return function(e) {
+    var time = getCurrentTime();
+    currentFormation.setTime(time, startOrEnd);
+
+    // this assumes that when you set a start time, the end time
+    // of the previous formation is already set -- meaning you
+    // can create the transition between the 2 formations
+    if (startOrEnd === 'start') {
+      var formation2 = currentFormation;
+      var formation1;
+      if (formationIndex === null) {
+        if (formationTimeline.formations.length) {
+          formation1 = formationTimeline.formations[formationTimeline.formations.length -1]
+        }
+      } else if (formationIndex > 0){
+        formation1 = formationTimeline.formations[formationIndex -1];
+      } else {
+        return;
+      }
+      var transition = new Transition(formation1, formation2);
+      formationTimeline.addTransition(transition);
+    }
+  }
 }
